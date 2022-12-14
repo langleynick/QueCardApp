92913395f0debaf2f4ef86171f559d8231995265
langleynick <w0106967@myscc.ca>
2022-12-14 00:32:50 -0500

    Changed styles on card output

--- QueCardApp/resources/js/script.js
+++ QueCardApp/resources/js/script.js
@@ -6,7 +6,7 @@ $(function() {
         let card = "";
         let question = $("#question").val();
         let answer = $("#answer").val();
-        card += "<div class='card m-4' style='width:450px'><h5 class='card-header'>Que Card</h5><div class='card-body'><h5 class='card-title'>Question</h5><p>" + question + "</p><h5 class='card-title'>Answer</h5><p>" + answer + "</p></div><div class='card-footer'><input type='checkbox' class='btn-check' id='answered' autocomplete='off'><label class='btn btn-outline-success' for='answered'>Complete</label></div></div>";
+        card += "<div class='card m-4' style='width:450px'><h5 class='card-header'>Que Card</h5><div class='card-body'><h5 class='card-title'><b>Question</b></h5><p class='h5'>" + question + "</p><h5 class='card-title'><b>Answer</b></h5><p class='h5'>" + answer + "</p></div><div class='card-footer'><input type='checkbox' class='btn-check' id='answered' autocomplete='off'><label class='btn btn-outline-success' for='answered'>Complete</label></div></div>";
         cardBox.append(card);
     });
 });
