20b0bc8d0947a890fac1e5be171e902332631f95
langleynick <w0106967@myscc.ca>
2022-12-14 22:30:58 -0500

    Fixed layout, changed script function
    Changed input to text area for more space
    Changed JS function for better scaling
    Added hide AND reveal button to fix toggle bug in cards
    Added reset on modal input so values are erased after submit

--- QueCardApp/resources/js/script.js
+++ QueCardApp/resources/js/script.js
@@ -1,30 +1,48 @@
 $(function() {
 
-    let cardBox = $("#queCardBox");
-    let cardCount = 0;
-    let queCards = [];
+    let $cardBox = $("#queCardBox");    //container for cards
+    let cardCount = 0;  //counter for card count
+    let queCards = [];  //array of que cards
 
-    //On submit of modal input
+    //on submit of modal input
     $('#submit').on('click', function(){
-        
-        let card = "";
 
-        cardCount++;
+        cardCount++;    //increase card count
+        $('#cardCount').text("Total Cards: " + cardCount);
 
         //get values from input tags
+        let $question = $("#question").val();
+        let $answer = $("#answer").val();
 
-        let question = $("#question").val();
-        let answer = $("#answer").val();
+        //create the card
+        let $cardContent = $("<div class='col card m-4'></div>");
 
-        //add tags and values into card var
+        //create header and append to card
+        let $cardHeader = $("<h5 class='card-header'>QueCard #" + cardCount + "</h5>");
+        $cardContent.append($cardHeader);
 
-        card += '<div class="col card m-4"><h5 class="card-header">Que Card #' + cardCount + '</h5><div class="card-body"><h5 class="card-title"><b><u>Question</u></b></h5><p class="h4 mb-4">' + question + '</p><button class="btn btn-dark mb-3 answer-reveal" type="button">Reveal Answer</button><div class="answer"><p>' + answer + '<p></div></div><div class="card-footer"><input type="checkbox" class="btn-check" id="' + cardCount + 'answered" autocomplete="off"><label class="btn btn-outline-success" for="' + cardCount + 'answered">Complete</label></div></div>';
+        //create body and append to card
+        let $cardBody = $("<div class='card-body'><h5 class='card-title'><b><u>Question</u></b></h5><p class='h4 mb-4'>" + $question + "</p><div><button class='btn btn-success mb-3 answer-reveal' type='button'>Reveal Answer</button><button class='btn btn-danger mx-1 mb-3 answer-hide'>Hide Answer</button></div><div class='answer'><p class='h4'>" + $answer + "<p></div></div>");
+        $cardContent.append($cardBody);
 
-        //add card into collape box
+        //create footer and append to card
+        let $cardFooter = $("<div class='card-footer'><input type='checkbox' class='btn-check' id='" + cardCount + "answered' autocomplete='off'><label class='btn btn-lg btn-outline-success' for='" + cardCount + "answered'>Complete</label></div></div>");
+        $cardContent.append($cardFooter);
 
-        cardBox.append(card);
+        //add card to the container 
+        $cardBox.append($cardContent);
+
+        //button event to reveal the answer
         $('button.answer-reveal').on('click', function(){
-            $(this).next().slideToggle();
+            $(this).parent().next().css("display", "block");
         });
+
+        //button event to hide the answer
+        $('button.answer-hide').on('click', function(){
+            $(this).parent().next().css("display", "none");
+        });
+
+        $("#question").val(" ");
+        $("#answer").val(" ");
     });
 });
