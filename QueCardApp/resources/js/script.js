964a5a91dba8eb6175cb544a2336d552c1332276
langleynick <w0106967@myscc.ca>
2022-12-14 21:26:08 -0500

    fixed column issues, added answer reveal
    Made que cards the entire width of the div instead of trying to append rows every few columns. Added a slideToggle function to a button to reveal the answer, so the answer is hidden at first glance

--- QueCardApp/resources/js/script.js
+++ QueCardApp/resources/js/script.js
@@ -1,22 +1,30 @@
 $(function() {
 
     let cardBox = $("#queCardBox");
+    let cardCount = 0;
+    let queCards = [];
 
     //On submit of modal input
     $('#submit').on('click', function(){
         
         let card = "";
 
+        cardCount++;
+
         //get values from input tags
 
         let question = $("#question").val();
         let answer = $("#answer").val();
+
         //add tags and values into card var
 
-        card += "<div class='card m-4' style='width:450px'><h5 class='card-header'>Que Card</h5><div class='card-body'><h5 class='card-title'><b>Question</b></h5><p class='h5'>" + question + "</p><h5 class='card-title'><b>Answer</b></h5><p class='h5'>" + answer + "</p></div><div class='card-footer'><input type='checkbox' class='btn-check' id='answered' autocomplete='off'><label class='btn btn-outline-success' for='answered'>Complete</label></div></div>";
+        card += '<div class="col card m-4"><h5 class="card-header">Que Card #' + cardCount + '</h5><div class="card-body"><h5 class="card-title"><b><u>Question</u></b></h5><p class="h4 mb-4">' + question + '</p><button class="btn btn-dark mb-3 answer-reveal" type="button">Reveal Answer</button><div class="answer"><p>' + answer + '<p></div></div><div class="card-footer"><input type="checkbox" class="btn-check" id="' + cardCount + 'answered" autocomplete="off"><label class="btn btn-outline-success" for="' + cardCount + 'answered">Complete</label></div></div>';
 
         //add card into collape box
 
         cardBox.append(card);
+        $('button.answer-reveal').on('click', function(){
+            $(this).next().slideToggle();
+        });
     });
 });
